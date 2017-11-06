88dd37979b6cf6bf29ec9f7d2f59cef994761739
Andriy Levkiv <andy.levkiv@gmail.com>
2017-11-06 17:06:08 +0000

    1st ed.

---
+++ js/index.js
@@ -0,0 +1,31 @@
+// alert('Hello World!'); 
+//console.log(`Hello World!`);
+//var helloWorld = 'Hello World!'
+//document.write(helloWorld);
+const userName = prompt('Eneter Your Name');
+document.writeln('Hello, ' + userName + ' !');
+let age = Number (prompt('Your Age'));
+prn('You Are: ' +  checkAge(age));
+age = age + 1;
+prn('Next Year You Will Be: ' + age);
+
+function prn(vaL) {
+    document.write('<br>' + vaL);
+}
+function checkAge(age)  {
+    if ((age >= 18) && (age<=60)) { return 'Adult';
+    }
+    else if (age < 18) { return 'Child';
+}
+return 'old';
+}
+
+
+
+
+
+
+
+
+
+
